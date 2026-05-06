4b342594962158b482d24155d052e644cbd1cfda
zebabegum765-prog <zebabegum765@gmail.com>
2026-05-06 18:08:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Portfolio Loaded Successfully 🚀");
+
+// Smooth scroll (if you add navigation later)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
+    });
+});
